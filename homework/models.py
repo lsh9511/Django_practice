5af3dd2354c7eccaf48605d6920c50d3f700711b
lsh9511 <iamshopsh@naver.com>
2025-07-30 17:42:21 +0900

    📝 5강 과제완료

--- homework/models.py
+++ homework/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class Todo(models.Model):
     title = models.CharField('제목',max_length=50)
     description = models.TextField('설명')
-    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField('시작일')
     end_date = models.DateField('마감일',)
     is_completed = models.BooleanField(default=False)
